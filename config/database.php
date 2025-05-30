98437ccf9a5f8720dc49147b6e0d8173e6fe48b5
claudio-nehemia <claudio.nehemia83@gmail.com>
2025-05-30 22:39:04 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -111,6 +111,17 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('MONGO_DB_HOST', '127.0.0.1'),
+            'port' => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE', 'juravest_db'),
+            'username' => env('MONGO_DB_USERNAME', ''),
+            'password' => env('MONGO_DB_PASSWORD', ''),
+            'options' => [
+                'database' => 'admin' // auth database jika ada
+            ]
+        ],
 
     ],
 
