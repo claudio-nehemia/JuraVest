eb5dedf5172331140a304dcd0f69816bff079c3d
Quacktonium <165171818+Quacktonium@users.noreply.github.com>
2025-06-01 22:39:56 +0700

    Add pengusaha CRUD

---
+++ app/Http/Controllers/PengusahaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pengusaha;
+use Illuminate\Http\Request;
+
+class PengusahaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pengusaha $pengusaha)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pengusaha $pengusaha)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pengusaha $pengusaha)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pengusaha $pengusaha)
+    {
+        //
+    }
+}
