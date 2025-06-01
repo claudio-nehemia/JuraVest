8409d950bed6e38efb11c6b620d1c4c17d1db189
claudio-nehemia <claudio.nehemia83@gmail.com>
2025-06-01 22:37:32 +0700

    add investor

---
+++ app/Http/Controllers/DataInvestorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataInvestor;
+use Illuminate\Http\Request;
+
+class DataInvestorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DataInvestor $dataInvestor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DataInvestor $dataInvestor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DataInvestor $dataInvestor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DataInvestor $dataInvestor)
+    {
+        //
+    }
+}
