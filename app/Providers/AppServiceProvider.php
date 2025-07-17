16013f757cc6118a1e7c59ed331b4cd533846041
claudio-nehemia <claudio.nehemia83@gmail.com>
2025-07-17 19:40:56 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(app()->environment('production')) {
+            URL::forceRootUrl(config('app.url')); // <- inject dari .env
             URL::forceScheme('https');
         }
     }
